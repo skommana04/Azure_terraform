e4e61745b0afe8089fa3d892116d93ea11f1b9c3
skommana04 <skommana04@gmail.com>
2026-02-16 07:17:30 -0600

    a

--- provider.tf
+++ provider.tf
@@ -14,7 +14,4 @@ terraform {
 provider "azurerm" {
   features {}
 
-  client_id       = "bc2ee156-191f-439a-ba8f-7f3ec15767db"
-  tenant_id       = "e2b45068-7762-4de3-95f9-d44c84e0e45d"
-  subscription_id = "7b8e0fae-d37a-4089-a683-cc71a03488c6"
 }

17989e2bd6cef35f3f158535540f0cbdc29d461a
skommana04 <skommana04@gmail.com>
2026-02-16 13:10:18 -0600

    a

--- varibles.tf
+++ varibles.tf
@@ -8,12 +8,13 @@
 variable "network_name" {
     type = string
     description = "network for roboshop"
+    default = "medha"
 }
 
-variable "subnet_name" {
-    type = string
-    description = "subnet for roboshop"
-}
- 
+variable "public_subnet_name" {}
+variable "private_subnet_name" {} 
 variable "address_space" {}     
-variable "address_prefixes" {}   
+variable "private_subnet_address_prefixes" {}  
+variable "public_subnet_address_prefixes" {}  
+variable "network_rules" {}
+variable "private_subnet_nsg_name" {}
